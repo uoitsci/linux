ab2caa7d8b77a021cbe6baa3b8997f89a0b957fe
Richard Drake <rdrake@rdrake.org>
2016-07-15 06:56:38 -0400

    Moved some installation/configuration steps from prepare.sh to the preseed file, fixed execution of prepare.sh script from within preseed file.

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,22 +7,17 @@
 
 set -e
 
-source ./util.sh
+base_path="$(dirname $0)"
+
+source ${base_path}/util.sh
 
 util::require_root
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 
-# Update the system using a local mirror.
-sed -i 's/ca.archive.ubuntu.com/mirror.science.uoit.ca/g' \
-  /etc/apt/sources.list
-
 # Update package list and upgrade any packages.
 apt-get update
 apt-get dist-upgrade -y
-
-# Install LibreOffice from the PPAs and some basic packages.
-apt-get install -y libreoffice uoit-laptop-fsci
 
 # Remove items that are no longer needed or would bloat the image.
 # The extra dist-upgrade and autoremove shouldn't be required, but
@@ -40,33 +35,6 @@ sed -i 's/mirror.science.uoit.ca/ca.archive.ubuntu.com/g' \
 # Update the package listing again so users don't try to connect
 # to our local mirror.
 apt-get update
-
-# Ubuntu's OEM install functionality is used to allow users to
-# configure their machines after they have received them.  If
-# certain values are not preseeded, users will be prompted to select
-# settings such as the keyboard layout.  We set these preseed values
-# so users are only prompted to create an account and name their
-# machine.
-
-# Set the keyboard layout to "us."
-echo "d-i keyboard-configuration/layoutcode string us" | \
-  debconf-set-selections
-
-# Set the timezone to "America/Toronto."
-echo "ubiquity time/zone string America/Toronto" | \
-  debconf-set-selections
-
-# Ask oem-config to only prompt users to create an account.
-echo "oem-config oem-config/steps multiselect user" | \
-  debconf-set-selections
-
-# Tell oem-config-firstboot to automatically populate preseeded
-# values.  If this is not set, users will be prompted to fill out
-# every value despite them already being set.  This is also
-# possible to achieve by adding "automatic-oem-config" to the boot
-# parameters.
-sed -i 's/automatic=$/automatic=--automatic/' \
-  /etc/init/oem-config.conf
 
 # The majority of users will want to continue to use the Windows
 # install provided by ITS.  By default, GRUB will boot into Ubuntu.
